c276d436e88c2c24c3f4345a6be043484d86d949
josephdaw <josephdaw@icloud.com>
2021-09-14 22:37:12 +0930

    setup basic skeleton

---
+++ script.js
@@ -0,0 +1,13 @@
+// give prompt for player to select R, P, or S.
+
+// use a random number generator to get the computer's choice for R, P, or S.
+
+// compare player and computer selections
+
+// R beats S
+
+// P beats R
+
+// S beats P
+
+// display result to the player
