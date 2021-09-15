bee62aeb3a22d46c3a864d3e2846a1b045a596f4
josephdaw <josephdaw@icloud.com>
2021-09-15 17:34:16 +0930

    add function for computer hand selection

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,7 +8,31 @@ function playGame() { if (window.confirm("Let's play a game...")) {
 };
 
 
+
+// generate at random either 0, 1, or 2
+function randomInt() {
+    return Math.floor(Math.random() * 3);
+};
+
+// declare variable for computers hand
+var computerHand;
+
 // use a random number generator to get the computer's choice for R, P, or S.
+function computerSelection () {
+    //get a random number
+    var x = randomInt();
+    // if randomInt = 0 then = Rock
+    if (x == 0) {
+        return computerHand = "R";
+    // if randomInt = 1 then = Paper
+    } else if (x == 1) {
+        return computerHand = "P";
+    // else = Scissors
+    } else {
+        return computerHand = "S";
+    }
+};
+
 
 // compare player and computer selections
 
