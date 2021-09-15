ba4a91357cc6734e76bb80aa1e8a0829a77ee1a5
josephdaw <josephdaw@icloud.com>
2021-09-15 17:09:23 +0930

    add click event to image to call playGame

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,12 @@
-// give prompt for player to select R, P, or S.
+
+// ask the player if they want to play
+function playGame() { if (window.confirm("Let's play a game...")) {
+    // give prompt for player to select R, P, or S.
+    window.prompt("Please select R, P, or S");
+    // check that a selection has been made
+};
+};
+
 
 // use a random number generator to get the computer's choice for R, P, or S.
 
