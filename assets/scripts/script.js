0743d44f7e0af22b632ea1fdc5c288fdb0572904
josephdaw <josephdaw@icloud.com>
2021-09-16 11:26:20 +0930

    clean up code, remove console.logs

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -18,8 +18,7 @@ function playGame() {
 
         // give prompt for player to select R, P, or S.
         playerHand = window.prompt("Please select R, P, or S").toUpperCase();
-        // playerHand = playerHand.toUpperCase;
-        console.log("player hand is " + playerHand);
+
         // check that a selection has been made
         // TO DO
 
@@ -29,13 +28,11 @@ function playGame() {
         //check who won
         result = whoWon();
 
-        console.log(winCount, lossCount, drawCount);
-
-        // display result to user
-
+        // display result to the player
         displayResult(result);
 
-       showTally();
+        // display results tally to the player
+        showTally();
 
     };
 }; // END - playGame
@@ -76,38 +73,30 @@ function computerSelection () {
 
 // compare player and computer selections
 function whoWon () {
-   //playerHand = "S";
-   //computerHand = "S";
-   // console.log(playerHand, computerHand);
 
     // both the same = draw
     if (playerHand == computerHand) {
         console.log("draw");
         drawCount ++;
-        console.log(drawCount);
         return result = "draw";
 
     // R beats S
     } else if (playerHand == "R" && computerHand == "S") {
-        console.log("win1");
         winCount ++; 
         return result = "win";
 
     // S beats P
     } else if (playerHand == "S" && computerHand == "P") {
-       console.log("win2");
        winCount ++;
        return result = "win";
 
     // P beats R 
     } else if (playerHand == "P" && computerHand == "R") {
-       console.log("win3");
        winCount ++; 
        return result = "win";
 
     // otherwise player looses
     } else {
-        console.log("loss");
         lossCount ++;
         return result = "loss";
     };
@@ -120,12 +109,15 @@ function displayResult (result) {
 
     if (result == "draw") {
         window.alert("It was a draw");
+
     } else if (result == "win") {
         window.alert("You won!");
+
     } else {
         window.alert("The computer won, better luck next time.");
     }
 } // END -- whoWon
+
 
 // show win loss draw tally
 function showTally () {
